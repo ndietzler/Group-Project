86cdd764045a9ef1d257be101aa5776d9d5934a9
Natasha Dietzler <ndietzler@gmail.com>
2015-12-02 11:51:08 -0800

    resize map

--- js/app.js
+++ js/app.js
@@ -6,56 +6,54 @@ var API_KEY = 'SOHI1JMKEKOSMGRC5';
 
 var myApp = angular.module('WorldApp', [])
 	.controller('WorldCtrl', ['$scope', '$http', function($scope, $http) {
-		//$scope.getBios = function() {
 			var request = BASE_URL + 'artist/biographies?' + 'api_key=' + API_KEY + '&name=Adele' + '&format=json';
 			console.log(request)
 			$http.get(request)
 			.then(function(response) {
 					console.log("...");
 					$scope.bio = response.data["response"]["status"]["version"];
-			}) 
-		//}
+	}) 
 }]);
 
-    $(function(){
-        $.getJSON('data/country.json', function(data) {
-            $('#map').highcharts('Map', {
-                title : {
-                    text : 'World Music'
-                },
+$(function(){
+    $.getJSON('data/country.json', function(data) {
+        $('#map').highcharts('Map', {
+            title : {
+                text : 'World Music'
+            },
 
-                subtitle : {
-                    text : 'Discover music of the world by clicking on a country...'
-                },
+            subtitle : {
+                text : 'Discover music of the world by clicking on a country...'
+            },
 
-                mapNavigation: {
-                    enabled: true,
-                    buttonOptions: {
-                        verticalAlign: 'bottom'
+            mapNavigation: {
+                enabled: true,
+                buttonOptions: {
+                    verticalAlign: 'bottom'
+                }
+            },
+            // colorAxis: {
+            //     min: 0
+            // },
+            colors: ['#000000'],
+
+            series : [{
+                data : data,
+                mapData: Highcharts.maps['custom/world-highres'],
+                joinBy: 'hc-key',
+                name: 'Random data',
+                states: {
+                    hover: {
+                        color: '#BADA55'
                     }
                 },
-                // colorAxis: {
-                //     min: 0
-                // },
-                colors: ['#000000'],
-
-                series : [{
-                    data : data,
-                    mapData: Highcharts.maps['custom/world-highres'],
-                    joinBy: 'hc-key',
-                    name: 'Random data',
-                    states: {
-                        hover: {
-                            color: '#BADA55'
-                        }
-                    },
-                    dataLabels: {
-                        enabled: false,
-                        //format: '{point.name}'
-                    }
-                }]
-            })
+                dataLabels: {
+                    enabled: false,
+                    //format: '{point.name}'
+                }
+            }]
         })
-    });
+    })
+});
 
 
