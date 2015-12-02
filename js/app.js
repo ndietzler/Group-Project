ec0933165189fa30a52d8878486b5105131fcab4
jkinsfat <jkinsfat@w.edu>
2015-12-02 13:56:51 -0800

    Added map styling and click events

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@
 var BASE_URL = 'http://developer.echonest.com/api/v4/';
 var CLIENT_ID = 'bf01b3b802764ec488bfda1ee9b29cd3';
 var API_KEY = 'SOHI1JMKEKOSMGRC5';
+var endpoint = 'http://developer.echonest.com/api/v4/'
+var apiKey = 'NO_API_KEY';
 
 var myApp = angular.module('WorldApp', [])
 	.controller('WorldCtrl', ['$scope', '$http', function($scope, $http) {
@@ -17,45 +19,64 @@ var myApp = angular.module('WorldApp', [])
 		//}
 }]);
 
-    $(function(){
-        $.getJSON('data/country.json', function(data) {
-            $('#map').highcharts('Map', {
-                title : {
-                    text : 'World Music'
+$(document).ready(function(){
+    $.getJSON('data/country.json', function(data) {
+        $('#map').highcharts('Map', {
+            title : {
+                style : {
+                    color : "white",
                 },
+                text : 'World Music'
+            },
 
-                subtitle : {
-                    text : 'Discover music of the world by clicking on a country...'
+            subtitle : {
+                style : {
+                    color : "white",
                 },
+                text : 'Discover music of the world by clicking on a country...'
+            },
 
-                mapNavigation: {
-                    enabled: true,
-                    buttonOptions: {
-                        verticalAlign: 'bottom'
-                    }
-                },
-                // colorAxis: {
-                //     min: 0
-                // },
-                colors: ['#000000'],
+            mapNavigation: {
+                enabled: true,
+                buttonOptions: {
+                    verticalAlign: 'bottom'
+                }
+            },
+            // colorAxis: {
+            //     min: 0
+            // },
+            colors: ['#23CF5F '],
 
-                series : [{
-                    data : data,
-                    mapData: Highcharts.maps['custom/world-highres'],
-                    joinBy: 'hc-key',
-                    name: 'Random data',
-                    states: {
-                        hover: {
-                            color: '#BADA55'
+            chart: {
+                backgroundColor: "#000000"
+            },
+            plotOptions:{
+                series:{
+                    point:{
+                        events:{
+                            click: function(){
+                                alert(this.name);
+                            }
                         }
-                    },
-                    dataLabels: {
-                        enabled: false,
-                        //format: '{point.name}'
                     }
-                }]
-            })
+                }
+            },
+            series : [{
+                data : data,
+                mapData: Highcharts.maps['custom/world-highres'],
+                joinBy: 'hc-key',
+                name: 'Random data',
+                borderColor: "#000000",
+                states: {
+                    hover: {
+                        color: '#F0FFFF'
+                    }
+                },
+                dataLabels: {
+                    enabled: false,
+                    //format: '{point.name}'
+                }
+            }]
         })
-    });
-
-
+    })
+});
