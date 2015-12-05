93afe653be60c6f8c54a61cdc631473f4850b19d
Isabella Spaletta <spaleisa@uw.edu>
2015-12-04 17:38:45 -0800

    getting top artists

--- js/app.js
+++ js/app.js
@@ -4,93 +4,106 @@ var SPOTIFY_BASE_URL = 'https://api.spotify.com/v1';
 var ECHO_NEST_BASE_URL = 'http://developer.echonest.com/api/v4/';
 var CLIENT_ID = 'bf01b3b802764ec488bfda1ee9b29cd3';
 var API_KEY = 'SOHI1JMKEKOSMGRC5';
+var fullName = "";
 
 var myApp = angular.module('WorldApp', [])
 	.controller('WorldCtrl', ['$scope', '$http', function($scope, $http) {
-		//$scope.getHotArtists = function() {
-			var request = ECHO_NEST_BASE_URL + 'artist/search?' + 'api_key=' + API_KEY + '&results=99' + '&artist_location=country:somalia' + "&sort=hotttnesss-desc" + "&bucket=hotttnesss&bucket=genre" + '&format=json';
+		$scope.getData = function(fullname) {
+			var request = ECHO_NEST_BASE_URL + 'artist/search?' + 'api_key=' + API_KEY + '&results=99' + '&artist_location=country:' + fullname + "&sort=hotttnesss-desc" + "&bucket=hotttnesss&bucket=genre" + '&format=json';
 			console.log(request)
 			$http.get(request)
 			.then(function(response) {
 					console.log("...");
-					$scope.topHot = response.data;
+                    var names = [];//
+                    $('#countryInfo table').html('<tr><th>Top 10 Artists</th></tr>');
+                    for (var i = 0; i < 10; i++) {
+                        var name = response.data["response"]["artists"][i]["name"];
+                        names.push(name);//
+                        $('#countryInfo table').append("<tr><td>" + name + "</td></tr>");
+                    }
+                    console.log(names);//
 			}) 
-		//}
-	}]);
+		}
+	
 
-$(document).ready(function(){
-    $.getJSON('data/country.json', function(data) {
-        $('#map').highcharts('Map', {
-            title : {
-                style : {
-                    color : "white",
+    $(document).ready(function(){
+        $.getJSON('data/country.json', function(data) {
+            $('#map').highcharts('Map', {
+                title : {
+                    style : {
+                        color : "white",
+                    },
+                    text : 'World Music'
                 },
-                text : 'World Music'
-            },
 
-            subtitle : {
-                style : {
-                    color : "white",
+                subtitle : {
+                    style : {
+                        color : "white",
+                    },
+                    text : 'Discover music of the world by clicking on a country...'
                 },
-                text : 'Discover music of the world by clicking on a country...'
-            },
 
-            mapNavigation: {
-                enabled: true,
-                buttonOptions: {
-                    verticalAlign: 'bottom'
-                }
-            },
+                mapNavigation: {
+                    enabled: true,
+                    buttonOptions: {
+                        verticalAlign: 'bottom'
+                    }
+                },
 
-            colors: ['#23CF5F '],
+                colors: ['#23CF5F '],
 
-            chart: {
-                backgroundColor: "#000000"
-            },
-            plotOptions:{
-                series:{
-                    point:{
-                        events:{
-                            click: function(){
-                                var fullName = this.name.toLowerCase();
-                                fullName = fullName.split(" ");
-                                if (fullName.length == 1) {
-                                    fullName = fullName[0];
-                                } else {
-                                    var urlName = '';
-                                    for (var i = 0; i < fullName.length; i++) {
-                                        urlName += fullName[i];
-                                        if (fullName[i + 1] != null) {
-                                            urlName += '+';
+                chart: {
+                    backgroundColor: "#000000"
+                },
+                plotOptions:{
+                    series:{
+                        point:{
+                            events:{
+                                click: function(){
+                                    fullName = this.name.toLowerCase();
+                                    fullName = fullName.split(" ");
+                                    if (fullName.length == 1) {
+                                        fullName = fullName[0];
+                                    } else {
+                                        var urlName = '';
+                                        for (var i = 0; i < fullName.length; i++) {
+                                            urlName += fullName[i];
+                                            if (fullName[i + 1] != null) {
+                                                urlName += '+';
+                                            }
+                                        }
+                                        fullName = urlName;
+                                        if (fullName == 'united+states+of+america') {
+                                            fullName = 'united+states';
                                         }
                                     }
-                                    fullName = urlName;
+                                    console.log(fullName);
+                                    $scope.getData(fullName);
                                 }
-                                console.log(fullName);
                             }
                         }
                     }
-                }
-            },
-
-            series : [{
-                data : data,
-                mapData: Highcharts.maps['custom/world-highres'],
-                joinBy: 'hc-key',
-
-                name: 'Random data',
-                borderColor: "#000000",
-
-                states: {
-                    hover: {
-                        color: '#F0FFFF'
-                    }
                 },
-                dataLabels: {
-                    enabled: false,
-                }
-            }]
+
+                series : [{
+                    data : data,
+                    mapData: Highcharts.maps['custom/world-highres'],
+                    joinBy: 'hc-key',
+
+                    name: 'Random data',
+                    borderColor: "#000000",
+
+                    states: {
+                        hover: {
+                            color: '#F0FFFF'
+                        }
+                    },
+                    dataLabels: {
+                        enabled: false,
+                    }
+                }]
+            })
         })
-    })
-});
+    });
+}]);
 
